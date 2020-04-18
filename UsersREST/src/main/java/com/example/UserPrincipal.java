20ac907896fed9a31cc13b928cfc5550eeb807ae
AhmedMostafa7474 <36231373+AhmedMostafa7474@users.noreply.github.com>
2020-04-18 14:12:18 +0200

    Add class UserPrincipal

---
+++ UsersREST/src/main/java/com/example/UserPrincipal.java
@@ -0,0 +1,68 @@
+package com.example;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserPrincipal implements UserDetails {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+    private String adress;
+    private String phone;
+    private List<GrantedAuthority> authorities;
+
+    public UserPrincipal(User user) {
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.id = user.getId();
+        this.adress = user.getAdress();
+        this.name = user.getName();
+        this.phone = user.getPhone();
+        this.authorities = Arrays.stream(user.getRoles().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
